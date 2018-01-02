fecc2d174306db8220ea930c0721f0e435ac1a39
Averac <desarrollo4@appsoga.com>
2018-01-02 14:18:53 -0500

    Commit en funcionamiento la web... antes de los archivos assets y sin webpack.

---
+++ src/Controller/PageController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: avera
+ * Date: 2/01/18
+ * Time: 01:40 PM
+ */
+
+namespace App\Controller;
+//
+//use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function index(){
+
+        return $this->render('indexPage.html.twig');
+    }
+}
