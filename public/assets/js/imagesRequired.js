836c182d86f60a29b73441534f531198c729ea85
Alejandro Vera Carrasquilla <avera02@misena.edu.co>
2018-03-20 16:56:57 -0500

    feat: cambio de links y logos

---
+++ public/assets/js/imagesRequired.js
@@ -0,0 +1,15 @@
+require('assets/img/logo.png');
+require('assets/img/h1.jpg');
+require('assets/img/h2.jpg');
+require('assets/img/h3.jpeg');
+require('assets/img/h4.jpeg');
+require('assets/img/hero.jpg');
+require('assets/img/l1.jpg');
+require('assets/img/l2.jpg');
+require('assets/img/l3.jpg');
+require('assets/img/l4.jpg');
+require('assets/img/l5.jpg');
+require('assets/img/l6.jpg');
+require('assets/img/l7.jpg');
+require('assets/img/l8.jpg');
+
